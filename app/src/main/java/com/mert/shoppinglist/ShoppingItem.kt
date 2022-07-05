970af7e53eb2623ed7d364f087785e41ceb41c18
Mert <218CS2101@isik.edu.tr>
2022-07-05 18:02:58 +0300

    ShoppingItem model is created

---
+++ app/src/main/java/com/mert/shoppinglist/ShoppingItem.kt
@@ -0,0 +1,17 @@
+package com.mert.shoppinglist
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items") // our table name
+data class ShoppingItem(
+    @ColumnInfo(name = "item_name") // our column name
+    var name: String,
+
+    @ColumnInfo(name = "item_amount") // our column name
+    var amount: Int
+){
+    @PrimaryKey(autoGenerate = true) // unique id for each item
+    var id: Int? = null // can be null
+}
