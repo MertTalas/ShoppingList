bcd9f0e863742e353111c095dfdba118a7a9787d
Mert <218CS2101@isik.edu.tr>
2022-07-05 18:32:57 +0300

    ShoppingDao and ShoppingDatabase implemented

---
+++ app/src/main/java/com/mert/shoppinglist/ShoppingDao.kt
@@ -0,0 +1,17 @@
+package com.mert.shoppinglist
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ShoppingDao {
+
+    @Insert (onConflict = OnConflictStrategy.REPLACE) // if database have this item new data replace with actual data
+    suspend fun upsert(item:ShoppingItem) // this method for both insert and update
+
+    @Delete
+    suspend fun delete(item: ShoppingItem) // delete shopping item func
+
+    @Query("SELECT * FROM shopping_items") // query for accessing all shopping items
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>> // for efficient update recyclerview we return with livedata our objects
+}
---
+++ app/src/main/java/com/mert/shoppinglist/ShoppingDatabase.kt
@@ -0,0 +1,30 @@
+package com.mert.shoppinglist
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShoppingItem::class], version = 1) // gives our entity names to database, and db version
+abstract class ShoppingDatabase: RoomDatabase() { // RoomDatabase is android sqlite database actually
+
+    abstract fun getShoppingDao(): ShoppingDao // for access db operations inside database class
+
+    companion object{ //companion==static in java, we create an instance for singleton using everywhere
+        @Volatile // writing on just one thread, we want don't to duplicate instance variable
+        private var instance: ShoppingDatabase?=null
+        private var LOCK= Any()
+
+        // this func executed always beginning of when create an ShoppingDatabase object
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){ // synchronized= no other threads can access that instance in the same time
+            instance?: createDatabase(context).also { // if instance is null creating new database and with "also" we set our new db to instance of it
+                instance=it
+            }
+        }
+
+        private fun createDatabase(context: Context) = Room.databaseBuilder( // RoomDatabase build func
+            context.applicationContext,
+            ShoppingDatabase::class.java,
+            "ShoppingDb.db").build()
+    }
+}
